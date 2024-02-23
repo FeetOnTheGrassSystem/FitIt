0a273a785241f547fa713f42d271975a14655978
JudeBautista <chichaybachoy0507@gmail.com>
2024-02-23 16:14:55 +0800

    Creating Folders for Forms

--- FitIt/FitIt/Program.cs
+++ FitIt/FitIt/Program.cs
@@ -1,3 +1,5 @@
+using FitIt.Dashboard;
+
 namespace FitIt
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace FitIt
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new DashboardForm());
         }
     }
 }
